e26c6c6f1698dd5248e0de9df20ff5317386051b
nikhilprashant <nikhilprashant1998@gmail.com>
2023-03-04 17:26:03 +0530

    Checked

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -57,16 +57,20 @@ public class ConnectionServiceImpl implements ConnectionService {
     }
     @Override
     public User communicate(int senderId, int receiverId) throws Exception {
-        User sender = userRepository2.findById(senderId).get();
-        User reciever = userRepository2.findById(receiverId).get();
-        String ip = reciever.getMaskedIp() == null? reciever.getOriginalIp(): reciever.getMaskedIp();
-        String code = ip.substring(0, 3);
-        if (code.equals(sender.getOriginalCountry().getCode())) return sender;
         try {
-            sender = connect(senderId, CountryName.valueOf(code).name());
-            return sender;
+            User sender = userRepository2.findById(senderId).get();
+            User reciever = userRepository2.findById(receiverId).get();
+            String ip = reciever.getMaskedIp() == null ? reciever.getOriginalIp() : reciever.getMaskedIp();
+            String code = ip.substring(0, 3);
+            if (code.equals(sender.getOriginalCountry().getCode())) return sender;
+            try {
+                sender = connect(senderId, CountryName.valueOf(code).name());
+                return sender;
+            } catch (Exception e) {
+                throw new Exception("Cannot establish communication");
+            }
         } catch (Exception e) {
-            throw new Exception("Cannot establish communication");
+            throw new Exception("Error in Communicate");
         }
     }
 }
